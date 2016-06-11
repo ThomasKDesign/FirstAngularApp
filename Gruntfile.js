27db3011cb8909ec4b345acca25950bb5860ed53
thomaskaiaua <thomaskaiaua@MacBook-Pro-3.local>
2016-06-10 20:42:14 -0700

    OpenWeatherMap weather app

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,6 +43,7 @@ module.exports = function (grunt) {
         options: {
           remote: 'git@github.com:thomaskdesign/firstAngularApp.git',
           branch: 'gh-pages'
+        
         }
       }
     },
